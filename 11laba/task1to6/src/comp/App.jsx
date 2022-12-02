cfe7c1abbb9dc3386721c2e0d959eac474a82243
uwulera <sofhblumorereal@mail.ru>
2022-12-03 01:10:03 +0700

    added 11 laba

---
+++ 11laba/task1to6/src/comp/App.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Heading"; 
+import List from './List';
+
+function App ()
+{
+    return(
+    <div>
+        <Heading />
+        <List />
+        <List />
+        </div>);
+}
+
+export default App;
